a0f02d667643e2a6d2bd65ed8ac88b9688a510b2
Danil Zaytsev <zaytsevdanil@yandex.ru>
2024-03-18 01:01:42 +0300

    ui-test

---
+++ story_cached/src/stories/Button.stories.js
@@ -0,0 +1,36 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
